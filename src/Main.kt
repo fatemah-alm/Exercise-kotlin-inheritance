358fd7635422209bc1c1cdd4820d821b2444a65e
fatemahAlMomen <fatemah.alm@hotmail.com>
2025-02-16 20:39:11 +0300

    Done part 5 and testing

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,10 @@
+import library.*
+
 fun main() {
-    println("Hello World!")
+    var myFictionBook = FictionBook("Harry Potter", "Jk.Rowling", 1234, "Fantasy")
+    var myNonFictionBook = NonFictionBook("AlOjairi Calender", "Saleh AlOjairy", 1155, "Science")
+
+
+    myFictionBook.displayInfo()
+    myNonFictionBook.displayInfo()
 }
--- src/library/FictionBook.kt
+++ src/library/FictionBook.kt
@@ -2,6 +2,6 @@ package library
 
 class FictionBook (title:String,author:String,ISBN:Int,var genre:String ):Book( title,  author,  ISBN){
     override fun  displayInfo(){
-        println("${displayInfo()} welcome to la la land, where dreams come true- $genre")
+        println("welcome to la la land, where dreams come true- $genre")
     }
 }
